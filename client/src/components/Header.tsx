645f9d5cb092e2ba87949cc643c44d0f2479fbd8
Lucas Santos <lucas.silva1203@outlook.com>
2023-02-28 14:30:39 -0300

    Add search input and user panel structure

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,31 @@
 import { Link } from "react-router-dom";
-import { List } from "phosphor-react";
+import { ArrowDown, CaretDown, List, MagnifyingGlass, UserList } from "phosphor-react";
 
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
 export const Header = () => {
     return (
         <header className={styles["header"]}>
-            <List size={38} weight="bold" color={"var(--secondary-color)"}/>
-            <h1 className={styles["header__logo"]}>
-                <Link to={"/"}>Electroware</Link>
-            </h1>
+            <div className={styles["header-main"]}>
+                {/* <List
+                    size={38}
+                    weight="bold"
+                    color={"var(--secondary-color)"}
+                /> */}
+                <h1 className={styles["header-logo"]}>
+                    <Link to={"/"}>Electroware</Link>
+                </h1>
+            </div>
+            <div className={styles["header-form"]}>
+                <form action="GET" name="search-input">
+                    <input type="text" />
+                    <input type="submit"/>
+                </form>
+            </div>
+            <div className={styles["header-user-panel"]}>
+                <UserList size={30}/>
+                <CaretDown size={30}/>
+            </div>
         </header>
     );
 };
