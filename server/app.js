4096ad6842260e809a843807ff9bca383de16214
Lucas Santos <lucas.silva1203@outlook.com>
2023-03-02 12:08:50 -0300

    Create server structure

--- server/app.js
+++ server/app.js
@@ -1,1 +1,19 @@
-console.log("hi")
+const connectDB = require("./middleware/db");
+const express = require("express");
+
+const userRouter = require("./routes/userRouter")
+const productRouter = require("./routes/productRouter")
+
+let app = express();
+
+app.listen(6060)
+connectDB();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// userRouter contains login and register router.
+app.use("/api", userRouter)
+
+//
+app.use("/api/product", productRouter)
