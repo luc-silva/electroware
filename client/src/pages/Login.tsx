66e939789f017b19cc48f46c443adb45da82658e
Lucas Santos <lucas.silva1203@outlook.com>
2023-03-02 20:51:38 -0300

    Add functional post requisitions and linter code

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,21 +1,54 @@
-import { Link } from "react-router-dom"
-import styles from "./Login.module.css"
+import axios from "axios";
+import { FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Login.module.css";
 
 export const Login = () => {
-  return (
-    <div className={styles["login"]}>
-      <div className={styles["login-text"]}>
-        <h1>Login</h1>
-        <p>Log in to your account and start spending your money!</p>
-      </div>
-      <div className={styles["login-form"]}>
-        <form action="POST">
-          <input type="email" placeholder="Email"/>
-          <input type="password" placeholder="Password" />
-          <input type="submit" value="Log In"/>
-        </form>
-        <Link to="/registration">Create an account</Link>
-      </div>
-    </div>
-  )
-}
+    let formInitialValue = {
+        email: "",
+        password: "",
+    };
+    let [form, setForm] = useState(formInitialValue);
+
+    function handleChange(event: FormEvent<HTMLFormElement>) {
+        let target = event.target;
+        if (target instanceof HTMLInputElement) {
+            setForm({ ...form, [target.name]: target.value });
+        }
+    }
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        axios.post("http://localhost:6060/api/login", form);
+    }
+
+    return (
+        <div className={styles["login"]}>
+            <div className={styles["login-text"]}>
+                <h1>Login</h1>
+                <p>Log in to your account and start spending your money!</p>
+            </div>
+            <div className={styles["login-form"]}>
+                <form
+                    action="POST"
+                    onChange={handleChange}
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        name="email"
+                        type="email"
+                        placeholder="Email"
+                        required
+                    />
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        required
+                    />
+                    <input type="submit" value="Log In" />
+                </form>
+                <Link to="/registration">Create an account</Link>
+            </div>
+        </div>
+    );
+};
--- client/src/pages/Registration.tsx
+++ client/src/pages/Registration.tsx
@@ -1,22 +1,76 @@
-import { Link } from "react-router-dom" 
-import styles from "./Registration.module.css"
+import { FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Registration.module.css";
+
+import axios from "axios";
 
 export const Registration = () => {
-  return (
-    <div className={styles["registration"]}>
-      <div className={styles["registration-text"]}>
-        <h1>Create an account</h1>
-        <p>Don't waste your money on other marketplaces anymore!</p>
-      </div>
-      <div className={styles["registration-form"]}>
-        <form action="POST">
-          <input type="text" placeholder="Your name"/>
-          <input type="email" placeholder="Email"/>
-          <input type="password" placeholder="Password" />
-          <input type="submit" value="Create an account"/>
-        </form>
-        <Link to="/login">Already have an account? Sign in </Link>
-      </div>
-    </div>
-  )
-}
+    let formInitialValues = {
+        first: "",
+        last: "",
+        email: "",
+        password: "",
+    };
+    let [form, setForm] = useState(formInitialValues);
+
+    function handleChange(event: FormEvent<HTMLFormElement>) {
+        let target = event.target;
+        if (target instanceof HTMLInputElement) {
+            setForm({ ...form, [target.name]: target.value });
+        }
+    }
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+      
+        event.preventDefault();
+        axios.post("http://localhost:6060/api/register", {
+            ...form,
+            name: { first: form.first, last: form.last },
+        });
+    }
+
+    return (
+        <div className={styles["registration"]}>
+            <div className={styles["registration-text"]}>
+                <h1>Create an account</h1>
+                <p>Don't waste your money on other marketplaces anymore!</p>
+            </div>
+            <div className={styles["registration-form"]}>
+                <form action="POST" onChange={handleChange} onSubmit={handleSubmit}>
+                    <input
+                        name="first"
+                        value={form.first}
+                        type="text"
+                        placeholder="First Name"
+                        required
+                    />
+                    <input
+                        name="last"
+                        value={form.last}
+                        type="text"
+                        placeholder="Last Name" 
+                    />
+                    <input
+                        name="email"
+                        value={form.email}
+                        type="email"
+                        placeholder="Email"
+                        required
+                    />
+                    <input
+                        name="password"
+                        value={form.password}
+                        type="password"
+                        placeholder="Password"
+                        required
+                    />
+                    <input
+                        type="submit"
+                        value="Crie uma conta"
+                    />
+                </form>
+                <Link to="/login">Already have an account? Sign in </Link>
+            </div>
+        </div>
+    );
+};
