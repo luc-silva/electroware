5ca97c2d3a976d75193a2b94c5591f6770beb86b
Lucas Santos <lucas.silva1203@outlook.com>
2023-03-02 12:08:20 -0300

    Fix auth token verification

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,22 +1,23 @@
-const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const asyncHandler = require("express-async-handler")
 const User = require("../models/User")
 
 const protected = asyncHandler(async(request, response, next) => {
-    if(!request.header.authorization && !request.header.authorization.startsWith("Bearer")){
+    if(!request.headers.authorization ||
+    !request.headers.authorization.startsWith("Bearer")){
         response.status(401)
         throw new Error("Nao autorizado")
     }
 
     //split the  "bearer" from the token
-    let token = request.header.authorization.split(" ")[1]
+    let token = request.headers.authorization.split(" ")[1]
 
     //decode the token to get the user id
-    let decoded = await bcrypt.verify(token, "123")
+    let decoded = jwt.verify(token, "123")
 
     //set the user in the response and send to the next middleware
-    response.user = await User.findById(decoded.id)
+    request.user = await User.findById(decoded.id)
     next()
 })
 
-module.exports = protected
+module.exports = {protected}
