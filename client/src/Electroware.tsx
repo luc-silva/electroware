cc54064317f6c75ee50d49e6bdfdf2c8fcd4dd12
Lucas Santos <lucas.silva1203@outlook.com>
2023-03-03 20:42:02 -0300

    Add a new route for search

--- client/src/Electroware.tsx
+++ client/src/Electroware.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header";
+import { createContext, useContext, useEffect, useState } from "react";
 
 import { Store } from "./pages/Store";
 import { Login } from "./pages/Login";
 import { Registration } from "./pages/Registration";
 import { Product } from "./pages/Product";
 import { Faq } from "./pages/Faq";
+import { SearchResults } from "./pages/SearchResults";
 
 function Electroware() {
     return (
@@ -13,12 +15,13 @@ function Electroware() {
             <Router>
                 <Header />
                 <Routes>
-                    <Route path="/" element={<Store />} />
-                    <Route path="/login" element={<Login />}/>
-                    <Route path="/registration" element={<Registration />}/>
-                    <Route path="/product" element={<Product/>}/>
-                    
-                    <Route path="/faq" element={<Faq />}/>
+                    <Route path="/home" element={<Store />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/registration" element={<Registration />} />
+                    <Route path="/product" element={<Product />} />
+                    <Route path="/search" element={<SearchResults />} />
+
+                    <Route path="/faq" element={<Faq />} />
                 </Routes>
             </Router>
         </div>
