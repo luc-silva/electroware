d9c7a86da0ac02f998c02408dc3197b4aaaf93c5
Lucas Santos <lucas.silva1203@outlook.com>
2023-03-06 14:19:54 -0300

    Add search page component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { MagnifyingGlass, ShoppingCart } from "phosphor-react";
 
 import styles from "./Header.module.css";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export const Header = () => {
     let isLogged = true;
+    let navigate = useNavigate()
+    let [searchInput, setSearchInput] = useState("");
+    const handleSearchInput = (event: ChangeEvent<HTMLInputElement>) => {
+        let target = event.target;
+        if (target) {
+            setSearchInput(target.value);
+        }
+    };
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        navigate(`/search/${searchInput}`)
+    }
 
     return (
         <header className={styles["header"]}>
@@ -14,23 +27,23 @@ export const Header = () => {
                 </h1>
             </div>
             <div className={styles["header__form"]}>
-                <form action="GET" name="search-input">
-                    <input type="text" />
-                    {/* <input type="submit" value={"Pesquisar"} /> */}
-                    <button>
+                <form action="POST" name="search-input" onSubmit={handleSubmit}>
+                    <input type="text"  value={searchInput} onChange={handleSearchInput} />
+                    <label className={styles["search-icon"]}>
+                        <input type="submit" value={"Pesquisar"} />
                         <MagnifyingGlass
-                            size={20}
-                            color="white"
-                            weight="bold"
-                        />
-                    </button>
+                                size={20}
+                                color="white"
+                                weight="bold"
+                            />
+                    </label>
                 </form>
             </div>
             <div className={styles["header__user-panel"]}>
                 {(isLogged && (
                     <div className={styles["header__user-panel__main--logged"]}>
                         <strong>Ola, Lucas</strong>
-                        <a href="">Ver perfil</a>
+                        <a href="/user/me">Ver perfil</a>
                     </div>
                 )) || (
                     <div className={styles["header__user-panel__main"]}>
