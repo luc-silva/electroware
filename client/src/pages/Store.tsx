109a1328d73e504deb8ac99f2794dc0f76607602
Lucas Santos <lucas.silva1203@outlook.com>
2023-03-07 11:56:05 -0300

    Translate home and fix CSS variables

--- client/src/pages/Store.tsx
+++ client/src/pages/Store.tsx
@@ -14,27 +14,31 @@ export const Store = () => {
             .then((response) => setRecentProducts(response.data));
     }, []);
     return (
-        <main role={"main"} className={styles["index"]}> 
+        <main role={"main"} className={styles["index"]}>
             <div>
-                <section className={styles["activepromos"]}></section>
+                <section className={styles["activepromos"]}>
+                    <img src="" alt="" />
+                </section>
             </div>
             <section className={styles["featured"]}>
-                <h2>Why should you buy with us?</h2>
+                <div>
+                    <h2>Por que você deveria comprar conosco?</h2>
+                </div>
                 <div className={styles["featured-container"]}>
                     <div className={styles["container-item"]}>
-                        Shipping for the whole world.
+                        Entrega para o mundo inteiro.
                         <Truck size={45} color={"var(--main-color)"} />
                     </div>
                     <div className={styles["container-item"]}>
-                        High quality boxes.
+                        Caixas de alta qualidade.
                         <Cube size={45} color={"var(--main-color)"} />
                     </div>
                     <div className={styles["container-item"]}>
-                        Trucks equipped with high-end engines.
+                        Caminhões equipados com motores de última geração.
                         <Gauge size={45} color={"var(--main-color)"} />
                     </div>
                     <div className={styles["container-item"]}>
-                        Safest payment methods available in the universe.
+                        Pagamento mais seguro do universo.
                         <Wallet size={45} color={"var(--main-color)"} />
                     </div>
                 </div>
@@ -43,18 +47,19 @@ export const Store = () => {
                 <div className={styles["products-container"]}></div>
             </section>
             <section className={styles["recent-products"]}>
-                <h2>Recent products</h2>
+                <h2>Recém anunciados</h2>
                 {recentProducts.length > 0 ? (
                     <ul className={styles["products-container"]}>
                         {recentProducts.length > 0
                             ? recentProducts.map(
                                   (
-                                      { name, price, owner },
+                                      { name, price, owner, _id }: Product,
                                       index: any,
                                       number
                                   ) => {
                                       return (
                                           <ProductCard
+                                              id={_id}
                                               name={name}
                                               price={price}
                                               key={index}
