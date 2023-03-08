30657c48bb4a927e9b2c7a8eb93f281ea31e04f7
Lucas Santos <lucas.silva1203@outlook.com>
2023-03-08 17:46:59 -0300

    Create user profile component

---
+++ client/src/pages/UserProfile.tsx
@@ -0,0 +1,56 @@
+import { Star } from "phosphor-react";
+import styles from "./UserProfile.module.css"
+
+export const UserProfile = () => {
+    return (
+        <main role={"main"} className={styles["user-profile"]} >
+            <aside className={styles["user-profile__info"]}>
+                <div className={styles["user-profile__picture"]}>
+                    <img src="" alt="" />
+                </div>
+                <div className={styles["user-profile__about"]}>
+
+                </div>
+            </aside>
+            <div className={styles["user-profile__main"]}>
+                <section className={styles["user-profile__details"]} >
+                    <div className={styles["user-profile__details__title"]}>
+                        <h2>Gabe Newelll</h2>
+                        <p>Sao Paulo, Santos</p>
+                    </div>
+                    <div className={styles["user-profile__details__reputation"]}>
+                        <p>Reputacao</p>
+                        <div>
+                            <strong>4.5</strong>
+                            <div className={[styles["user-profile__details__reputation"], styles["stars-container"]].join("")}>
+                                <Star size={20} weight="duotone"  color="var(--main-color)"/>
+                                <Star size={20} weight="duotone" color="var(--main-color)" />
+                                <Star size={20} weight="duotone" color="var(--main-color)"/>
+                                <Star size={20} weight="duotone" color="var(--main-color)" />
+                                <Star size={15} color="var(--main-color)"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles["user-description"]}>
+                        <div className={styles["description-title"]}>
+                            <p>Descricao</p>
+                        </div>
+                        <div className={styles["description-text"]}>
+
+                        </div>
+                    </div>
+                </section>
+                <section className={styles["user-profile__products"]}>
+                    <div className={styles["user-profile__products__title"]}>
+                        <h2>Produtos a venda</h2>
+                    </div>
+                    <div className={styles["user-profile__products__container"]}>
+                        <div>
+                
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+    );
+};
