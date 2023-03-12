5f2a7c63569c185bd2b302f506eeb837545e4a32
Lucas Santos <lucas.silva1203@outlook.com>
2023-03-12 14:54:26 -0300

    Create Checkout page component

--- client/src/pages/Checkout.tsx
+++ client/src/pages/Checkout.tsx
@@ -1,7 +1,27 @@
+import styles from "./Checkout.module.css"
+import axios from "axios"
+
 export const Checkout = () => {
+    function handleCheckout(){
+        axios.
+    }
+
     return (
-        <main>
-            
+        <main className={styles["checkout"]}>
+            <article className={styles["checkout__main"]}>
+                <div className={styles["checkout__total-value"]}>
+                    <p>Valor Total:</p>
+                    <strong>40.00 R$</strong>
+                </div>
+                <div className={styles["checkout__footer"]}>
+                    <div>
+                        <p>FORMA DE PAGAMENTO: BOLETO</p>
+                    </div>
+                    <div>
+                        <button onClick={handleCheckout}>FINALIZAR COMPRA</button>
+                    </div>
+                </div>
+            </article>
         </main>
     )
 }
