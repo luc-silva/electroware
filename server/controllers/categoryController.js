0bcdaeb3242c37e8e7a307c23b99e1201ae09b84
Lucas Santos <lucas.silva1203@outlook.com>
2023-03-16 11:09:09 -0300

    Fix category validation

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,25 +1,32 @@
-const asyncHandler = require("express-async-handler")
-const Category = require("../models/Category")
+const asyncHandler = require("express-async-handler");
+const Category = require("../models/Category");
 
-const createCategory = asyncHandler( async (request, response) => {
-    if(!request.body){
-        response.status(400)
-        throw new Error({message:"Por favor, insira os dados requeridos"})
+const createCategory = asyncHandler(async (request, response) => {
+    if (!request.body.name) {
+        response.status(400);
+        throw new Error("Por favor, insira os dados requeridos");
     }
-    let category = await Category.create(request.body)
-    response.json(category)
-})
-const getCategories = asyncHandler( async(request, response) => {
-    const categories = await Category.find()
-    if(categories.length === 0 ){
-        response.status(404)
-        response.json({message: "nenhuma categoria foi encontrada"})
+    let categoryExists = await Category.find({ name: request.body.name });
+    if (categoryExists) {
+        response.status(400);
+        throw new Error("Categoria ja existente");
     }
-    response.status(200)
-    response.json(categories)
-})
+    
+    let category = await Category.create(request.body);
+    response.json(category);
+});
+
+const getCategories = asyncHandler(async (request, response) => {
+    const categories = await Category.find();
+    if (categories.length === 0) {
+        response.status(404);
+        response.json({ message: "nenhuma categoria foi encontrada" });
+    }
+    response.status(200);
+    response.json(categories);
+});
 
 module.exports = {
     createCategory,
     getCategories,
-}
+};
