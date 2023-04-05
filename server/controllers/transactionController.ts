c3be244cf22646797efdcbf528ea5151125aa12e
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-05 17:33:21 -0300

    Improves Transaction backend validation
    Creates class responsible for the validation
    
    This commit is part of issue #5

--- server/controllers/transactionController.ts
+++ server/controllers/transactionController.ts
@@ -8,112 +8,114 @@ import User from "../models/User";
 import Transaction from "../models/Transaction";
 import { Request, Response } from "express";
 import { IUser } from "../interface";
+import TransactionValidator from "../validators/TransactionValidator";
 
 //post
-export const createProductTransaction = asyncHandler(async (request: Request, response: Response)  => {
-    if(!request.body || !request.user){
-        response.status(400)
-        throw new Error("Dados Inválidos")
-    }
+export const createProductTransaction = asyncHandler(
+    async (request: Request, response: Response) => {
+        if (!request.body || !request.user) {
+            response.status(400);
+            throw new Error("Dados Inválidos");
+        }
 
-    let user = await User.findById(request.user) as IUser;
-    if (!user) {
-        response.status(404);
-        throw new Error("Usuário não encontrado.");
-    }
+        let user = (await User.findById(request.user)) as IUser;
+        if (!user) {
+            response.status(404);
+            throw new Error("Usuário não encontrado.");
+        }
 
-    let {paymentMethod} = request.body
-    if(typeof paymentMethod !== "string"){
-        response.status(400)
-        throw new Error("Método de pagamento inválido.")
-    }
+        TransactionValidator.validate(response, request.body);
+        let { paymentMethod } = request.body;
 
-    let productsBought = await ProductInstance.find({ user: user.id });
-    if (productsBought.length === 0) {
-        response.status(404);
-        throw new Error("Não há produtos no carrinhos de compras.");
-    }
+        let productsBought = await ProductInstance.find({ user: user.id });
+        if (productsBought.length === 0) {
+            response.status(404);
+            throw new Error("Não há produtos no carrinhos de compras.");
+        }
 
-    function getTotal() {
-        let total = 0;
-        productsBought.forEach(({ price, quantity }) => {
-            total += price * quantity;
-        });
-        return total;
-    }
+        function getTotal() {
+            let total = 0;
+            productsBought.forEach(({ price, quantity }) => {
+                total += price * quantity;
+            });
+            return total;
+        }
 
-    if (user.funds < getTotal()) {
-        response.status(400);
-        throw new Error("Fundos insuficientes.");
-    }
+        if (user.funds < getTotal()) {
+            response.status(400);
+            throw new Error("Fundos insuficientes.");
+        }
 
-    try {
-        const session = await mongoose.startSession();
-        await session.withTransaction(async () => {
-            let data = {
-                buyer: user.id,
-                paymentMethod,
-                productsBought,
-                totalPrice: getTotal(),
-            };
-            let transaction = await Transaction.create([data], { session });
+        try {
+            const session = await mongoose.startSession();
+            await session.withTransaction(async () => {
+                let data = {
+                    buyer: user.id,
+                    paymentMethod,
+                    productsBought,
+                    totalPrice: getTotal(),
+                };
+                let transaction = await Transaction.create([data], { session });
 
-            await User.findByIdAndUpdate(
-                [user.id],
-                { $inc: { funds: -getTotal() } },
-                { session }
-            );
-
-            for (let productInstance of productsBought) {
                 await User.findByIdAndUpdate(
-                    [productInstance.seller],
-                    {
-                        $inc: {
-                            funds: +(
-                                productInstance.price * productInstance.quantity
-                            ),
-                        },
-                    },
+                    [user.id],
+                    { $inc: { funds: -getTotal() } },
                     { session }
                 );
-                await Product.findByIdAndUpdate([productInstance.product], {
-                    $inc: { quantity: -productInstance.quantity },
-                });
 
-                await ProductInstance.findByIdAndDelete(productInstance.id);
-            }
+                for (let productInstance of productsBought) {
+                    await User.findByIdAndUpdate(
+                        [productInstance.seller],
+                        {
+                            $inc: {
+                                funds: +(
+                                    productInstance.price *
+                                    productInstance.quantity
+                                ),
+                            },
+                        },
+                        { session }
+                    );
+                    await Product.findByIdAndUpdate([productInstance.product], {
+                        $inc: { quantity: -productInstance.quantity },
+                    });
 
-            await session.commitTransaction();
-            response.status(201).json(transaction);
-        });
-        session.endSession();
-    } catch (error) {
-        response.status(400);
-        throw new Error(`Algo deu errado: ${error}.`);
+                    await ProductInstance.findByIdAndDelete(productInstance.id);
+                }
+
+                await session.commitTransaction();
+                response.status(201).json(transaction);
+            });
+            session.endSession();
+        } catch (error) {
+            response.status(400);
+            throw new Error(`Algo deu errado: ${error}.`);
+        }
     }
-});
-
+);
 
 ////private
 
 //get, need params
-export const getUserTransactions = asyncHandler(async (request: Request, response: Response)  => {
-    if(!request.user || !request.params){
-        response.status(400)
-        throw new Error("Dados Inválidos.")
-    }
+export const getUserTransactions = asyncHandler(
+    async (request: Request, response: Response) => {
+        if (!request.user || !request.params) {
+            response.status(400);
+            throw new Error("Dados Inválidos.");
+        }
 
-    let user = await User.findById(request.user);
-    if (!user) {
-        response.status(404);
-        throw new Error("Usuário não encontrado.");
-    }
+        let user = await User.findById(request.user);
+        if (!user) {
+            response.status(404);
+            throw new Error("Usuário não encontrado.");
+        }
 
-    if (user.id !== request.params.id) {
-        response.status(401);
-        throw new Error("Não autorizado.");
-    }
+        if (user.id !== request.params.id) {
+            response.status(401);
+            throw new Error("Não autorizado.");
+        }
 
-    let transactions = await Transaction.find({ buyer: user._id });
-    response.status(200).json(transactions);
-});
+        let transactions = await Transaction.find({ buyer: user._id });
+        response.status(200).json(transactions);
+    }
+);
