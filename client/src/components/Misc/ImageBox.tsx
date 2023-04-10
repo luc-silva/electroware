e25bed742afefccafa0f20aa4e59477ee818d134
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-10 14:01:59 -0300

    Add ImageInput component and fix img sizings

--- client/src/components/Misc/ImageBox.tsx
+++ client/src/components/Misc/ImageBox.tsx
@@ -3,15 +3,17 @@ import styles from "./ImageBox.module.css";
 export const ImageBox = ({
     isLoading,
     alt = "",
+    imgSrc
 }: {
     isLoading: boolean;
+    imgSrc?: string
     alt?: string;
 }) => {
     //let image = require("")
     return (
         <div className={styles["image-container"]}>
             {(isLoading && <div className={styles["loading-thumb"]} />) || (
-                <img src="" alt={alt} loading={"lazy"} />
+                <img src={imgSrc} alt={alt} loading={"lazy"} />
             )}
         </div>
     );
