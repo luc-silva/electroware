e41213f340567b3c832a79bf63aad2a6c610e713
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-12 16:22:15 -0300

    Improve imageUploader middleware

--- server/middleware/buffer.ts
+++ server/middleware/buffer.ts
@@ -1,19 +1,34 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, response, Response } from "express";
 import asyncHandler from "express-async-handler";
 import multer from "multer";
-
+import { extname } from "path";
 const storage = multer.memoryStorage();
 const upload = multer({
     storage: storage,
     limits: {
         fileSize: 4e6, //4 MB
     },
-}).single("imageFile");
+    fileFilter: (req, file, callback) => {
+        const allowed = /jpg|png|jpeg/;
+        let ext = allowed.test(extname(file.originalname));
+        if (ext) {
+            return callback(null, true);
+        } else {
+            callback(new Error("Imagem Inválida"));
+        }
+    },
+}).single("userImage");
 
 export const imageUploader = asyncHandler(
     async (request: Request, response: Response, next: NextFunction) => {
         upload(request, response, (err) => {
-            next();
+            if (err) {
+                response.status(400).json({ message: err.message });
+            } else {
+                console.log(request.body);
+                console.log(request.file);
+                next();
+            }
         });
     }
 );
