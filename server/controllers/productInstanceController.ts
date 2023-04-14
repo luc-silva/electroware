56f535c87d30492bed9d016e0ad8dccdb8cd2985
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-14 19:18:27 -0300

    Modify getSingleProductInstance
    This function now fetch for aditional data about its references
    
    This commit is part of issue #13

--- server/controllers/productInstanceController.ts
+++ server/controllers/productInstanceController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import mongoose from "mongoose";
 import { IProductInstance } from "../interface";
+import ImageInstance from "../models/ImageInstance";
 import Product from "../models/Product";
 import ProductInstance from "../models/ProductInstance";
 import User from "../models/User";
@@ -17,7 +18,7 @@ export const createInstance = asyncHandler(
             throw new Error("Dados Inválidos.");
         }
 
-        ProductInstanceValidator.validate(response, request.body)
+        ProductInstanceValidator.validate(response, request.body);
         let { user, product, price, quantity }: IProductInstance = request.body;
 
         let instanceOwner = await User.findById(request.user);
@@ -40,6 +41,14 @@ export const createInstance = asyncHandler(
             throw new Error("Você não pode comprar o seu próprio produto.");
         }
 
+        let productImage = await ImageInstance.findOne({
+            product: instanceProduct.id,
+        });
+        if (!productImage) {
+            response.status(400);
+            throw new Error("O Produto não possui imagem.");
+        }
+
         let instanceAlreadyExist = await ProductInstance.findOne({
             product: instanceProduct.id,
             user: instanceOwner.id,
@@ -55,6 +64,7 @@ export const createInstance = asyncHandler(
             price,
             quantity,
             seller: instanceProduct.owner,
+            productImage: productImage.id,
         });
 
         response.status(201).json(createdInstance);
@@ -136,9 +146,16 @@ export const getSingleInstance = asyncHandler(
             throw new Error("Dados Inválidos.");
         }
 
-        let instance = await ProductInstance.findById(id).select({product: 1, price: 1, quantity: 1})
+        let instance = await ProductInstance.findById(id)
+            .select({ product: 1, price: 1, quantity: 1 })
+            .populate("seller", { name: 1 })
+            .populate("product", { id: 1, category: 1, name: 1, owner: 1 })
+            .populate("productImage", { data: 1 });
+        if (!instance) {
+            response.status(400);
+            throw new Error("Item Não Encontrado.");
+        }
 
-        response.status(200).json(instance)
-
+        response.status(200).json(instance);
     }
 );
--- server/models/ProductInstance.ts
+++ server/models/ProductInstance.ts
@@ -4,6 +4,7 @@ const ProductInstanceSchema = new Schema({
     user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
     seller: { type: Schema.Types.ObjectId, required: true, ref: "User" },
     product: { type: Schema.Types.ObjectId, required: true, ref: "Product" },
+    productImage: { type: Schema.Types.ObjectId, required: true, ref: "Image" },
     price: { type: Number, required: true },
     quantity: { type: Number, required: true },
 });
