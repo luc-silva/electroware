be6ac5754ed5dafa1dc9ed9cf94cdb48a00b085b
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-14 17:59:54 -0300

    Improve SearchResultItem component
    Component now loads the product image.
    Also fix product details not being showed correctly

--- client/src/components/SearchResultItem.tsx
+++ client/src/components/SearchResultItem.tsx
@@ -1,30 +1,57 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import ProductService from "../services/ProductService";
 import UserService from "../services/UserService";
+import { createImage } from "../utils/operations";
+import { ImageBox } from "./Misc/ImageBox";
 import styles from "./SearchResultItem.module.css";
 
-export const SearchResultItem = ({ product }: { product: Product }) => {
+export const SearchResultItem = ({ productId }: { productId: string }) => {
+    const productInitialState = {
+        product: {
+            price: 0,
+            name: "",
+            owner: "",
+            _id: "",
+        },
+        image: { data: null as null | string },
+    };
+    let [status, setStatus] = useState(true);
     let [seller, setSeller] = useState({ first: "", last: "" });
+    let [productData, setProductData] = useState(productInitialState);
+
     useEffect(() => {
-        UserService.getUserInfo(product.owner)
+        ProductService.getProductDetails(productId)
             .then((data) => {
-                //type it later
-                setSeller({ first: data.name.first, last: data.name.last });
+                setProductData(data);
+            })
+            .then(() => {
+                setStatus(false);
             });
     }, []);
+    useEffect(() => {
+        UserService.getUserInfo(productData.product.owner).then(({ name }) => {
+            console.log({ name });
+            setSeller(name);
+        });
+    }, [productData.product._id]);
+
     return (
         <div className={styles["result-product"]}>
-            <Link to={`/product/${product._id}`}>
+            <Link to={`/product/${productData.product._id}`}>
                 <div className={styles["product-image"]}>
-                    <img src="a" alt="" />
+                    <ImageBox
+                        isLoading={status}
+                        imgSrc={createImage(productData.image.data)}
+                    />
                 </div>
                 <div className={styles["product-details"]}>
                     <div className={styles["product-details__main"]}>
-                        <h3>{product.name}</h3>
+                        <h3>{productData.product.name}</h3>
                         <p>Vendido por {`${seller.first} ${seller.last}`}</p>
                     </div>
                     <div className={styles["product-details__info"]}>
-                        <strong>{`R$ ${product.price}`}</strong>
+                        <strong>{`R$ ${productData.product.price}`}</strong>
                     </div>
                 </div>
             </Link>
