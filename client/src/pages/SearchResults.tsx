950c595e6c1d6dca67c87335a514ba2243b259ba
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-14 17:58:42 -0300

    Break page into subcomponents

--- client/src/pages/SearchResults.tsx
+++ client/src/pages/SearchResults.tsx
@@ -4,19 +4,16 @@ import { useEffect, useState } from "react";
 import { SearchResultItem } from "../components/SearchResultItem";
 import ProductService from "../services/ProductService";
 import CategoryService from "../services/CategoryService";
+import { CategoriesPanel } from "../components/Misc/CategoriesPanel";
 
 export const SearchResults = () => {
     let { search } = useParams();
     let [searchResults, setSearchResults] = useState([]);
-    let [categories, setCategories] = useState([]);
-    
+
     useEffect(() => {
         if (search) {
             ProductService.searchProduct(search).then((data) => {
                 setSearchResults(data);
-            });
-            CategoryService.getCategories().then((data) => {
-                setCategories(data);
             });
         }
     }, [search]);
@@ -28,23 +25,9 @@ export const SearchResults = () => {
                     <h3>Filtros</h3>
                     <p>Ainda em Implementação</p>
                 </section> */}
-                <section className={styles["category-container"]}>
-                    <div>
-                        <h3>Categorias</h3>
-                    </div>
-                    <ul>
-                        {categories.map(
-                            (
-                                { name, _id }: { name: string; _id: string },
-                                index: React.Key
-                            ) => (
-                                <li key={index}>
-                                    <Link to={`/category/${_id}`}>{name}</Link>
-                                </li>
-                            )
-                        )}
-                    </ul>
-                </section>
+                <div className={styles["categories-container"]}>
+                    <CategoriesPanel />
+                </div>
             </aside>
 
             <section className={styles["search-results__main"]}>
@@ -53,8 +36,8 @@ export const SearchResults = () => {
                     <p>{searchResults.length} resultados</p>
                 </div>
                 <div className={styles["results-container"]}>
-                    {searchResults.map((product: Product, index: React.Key) => (
-                        <SearchResultItem product={product} key={index} />
+                    {searchResults.map((id: string, index: React.Key) => (
+                        <SearchResultItem productId={id} key={index} />
                     ))}
                 </div>
             </section>
