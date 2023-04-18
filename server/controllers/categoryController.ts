8fe8950ee1b6a80fb90926f555b9f7927796fbac
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-18 18:31:14 -0300

    Add JSDoc for categoryController endpoints
    This commit is part of issue #15

--- server/controllers/categoryController.ts
+++ server/controllers/categoryController.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
+import { Types } from "mongoose";
 import Category from "../models/Category";
 
 interface ICategoryDTO {
     name: string;
 }
 
-//post
+/**
+ * POST - Create a category for products.
+ *
+ * @param {Request} request - The HTTP request object containing category name.
+ * @param {Response} response - The HTTP response object containing a conclusion message.
+ * @throws throws error if receives a invalid data and if a category has already been created.
+ */
 export const createCategory = asyncHandler(
     async (request: Request, response: Response) => {
         if (!request.body) {
@@ -26,24 +33,35 @@ export const createCategory = asyncHandler(
             throw new Error("Categoria já existente.");
         }
 
-        let categoryCreated = await Category.create(request.body);
-        response.status(201).json(categoryCreated);
+        await Category.create(request.body);
+        response.status(201).json({message: "Categoria Criada."});
     }
 );
 
-//get
+/**
+ * Get - Get every products categories.
+ *
+ * @param {Request} request - HTTP request doesn't requires any params or data.
+ * @param {Response} response - The HTTP response object containing a conclusion message.
+ * @throws throws error only with unexpected issues.
+ */
 export const getCategories = asyncHandler(
     async (request: Request, response: Response) => {
         const categories = await Category.find();
         if (categories.length === 0) {
-            response.status(404);
-            throw new Error("Nenhuma categoria encontrada.");
+            response.status(404).json({message: "Nenhuma categoria encontrada."});
         }
         response.status(200).json(categories);
     }
 );
 
-//get, need id
+/**
+ * Get - Get a single category with given id. It should be a valid ObejctId
+ *
+ * @param {Request} request - The HTTP request object containing category id.
+ * @param {Response} response - The HTTP response object containing category info.
+ * @throws throws error if receives a invalid data and if a category has not been found.
+ */
 export const getSingleCategory = asyncHandler(
     async (request: Request, response: Response) => {
         if (!request.params) {
@@ -52,9 +70,9 @@ export const getSingleCategory = asyncHandler(
         }
 
         let { id } = request.params;
-        if(typeof id !== "string"){
+        if (!Types.ObjectId.isValid(id)) {
             response.status(400);
-            throw new Error("URL Inválida")
+            throw new Error("Dados Inválidos");
         }
 
         let category = await Category.findById(id);
