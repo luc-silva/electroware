c436a1fede0d1f3fd07a5d8e5c82c9362e188a01
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-18 17:46:30 -0300

    Add JSDoc for reviewsController endpoint
    This commit is part of issue #15

--- server/controllers/reviewsController.ts
+++ server/controllers/reviewsController.ts
@@ -6,9 +6,15 @@ import Product from "../models/Product";
 import { IReview, IUser } from "../interface";
 import { Request, Response } from "express";
 import ReviewValidator from "../validators/ReviewValidator";
+import { Types } from "mongoose";
 
-////public
-//get, need params
+/**
+ * GET - Get a review with given review id. It should be a valid ObjectId.
+ *
+ * @param {Request} request - The HTTP request object containing the review ID.
+ * @param {Response} response - The HTTP response object containing the review info.
+ * @throws throws error if review id isn't valid or if the review has not been found.
+ */
 export const getSingleReview = asyncHandler(
     async (request: Request, response: Response) => {
         if (!request.params) {
@@ -17,20 +23,28 @@ export const getSingleReview = asyncHandler(
         }
 
         let { id } = request.params;
+        if (!Types.ObjectId.isValid(id)) {
+            response.status(400);
+            throw new Error("Dados Inválidos");
+        }
 
-        let review = await Review.findById(id).populate(
-            "author",
-            { name: 1 }
-        );
-        if(!review){
-            response.status(404)
-            throw new Error("Avaliação não encontrada.")
+        let review = await Review.findById(id).populate("author", { name: 1 });
+        if (!review) {
+            response.status(404);
+            throw new Error("Avaliação não encontrada.");
         }
 
         response.status(200).json(review);
     }
 );
 
+/**
+ * GET - Get product reviews with given product id. It should be a valid ObjectId.
+ *
+ * @param {Request} request - The HTTP request object containing the product ID.
+ * @param {Response} response - The HTTP response object containing the review ids or a message.
+ * @throws throws error if product id isn't valid or if the product has not been found.
+ */
 export const getProductReviews = asyncHandler(
     async (request: Request, response: Response) => {
         if (!request.params) {
@@ -39,6 +53,11 @@ export const getProductReviews = asyncHandler(
         }
 
         let { id } = request.params;
+        if (!Types.ObjectId.isValid(id)) {
+            response.status(400);
+            throw new Error("Dados Inválidos");
+        }
+
         let product = await Product.findById(id);
         if (!product) {
             response.status(404);
@@ -60,7 +79,13 @@ export const getProductReviews = asyncHandler(
     }
 );
 
-//get, need params
+/**
+ * GET - Get every user reviews made with given user id. It should be a valid ObjectId.
+ *
+ * @param {Request} request - The HTTP request object containing the user ID.
+ * @param {Response} response - The HTTP response object containing the review ids or a message.
+ * @throws throws error if user id isn't valid or if the user has not been found.
+ */
 export const getEveryUserReviews = asyncHandler(
     async (request: Request, response: Response) => {
         if (!request.params) {
@@ -69,23 +94,33 @@ export const getEveryUserReviews = asyncHandler(
         }
 
         let { id } = request.params;
+        if (!Types.ObjectId.isValid(id)) {
+            response.status(400);
+            throw new Error("Dados Inválidos");
+        }
+
         let user = await User.findById(id);
         if (!user) {
             response.status(404);
             throw new Error("Usuário não encontrado.");
         }
 
-        let reviews = await Review.find({ author: user.id });
+        let reviews = await Review.find({ author: user.id }).select({ id: 1 });
         if (reviews.length === 0) {
-            response.status(404);
-            throw new Error("Sem análises disponíveis.");
+            response.status(200).json({ message: "Sem análises disponíveis." });
         }
 
         response.status(200).json(reviews);
     }
 );
 
-//get, need params
+/**
+ * GET - Get reviews from a product where the user is the owner with given user id. It should be a valid ObjectId.
+ *
+ * @param {Request} request - The HTTP request object containing the user ID.
+ * @param {Response} response - The HTTP response object containing the review ids or a message.
+ * @throws throws error if user id isn't valid or if the user has not been found.
+ */
 export const getEveryUserProductsReviews = asyncHandler(
     async (request: Request, response: Response) => {
         if (!request.params) {
@@ -94,6 +129,10 @@ export const getEveryUserProductsReviews = asyncHandler(
         }
 
         let { id } = request.params;
+        if (!Types.ObjectId.isValid(id)) {
+            response.status(400);
+            throw new Error("Dados Inválidos");
+        }
 
         let user = await User.findById(id);
         if (!user) {
@@ -104,17 +143,20 @@ export const getEveryUserProductsReviews = asyncHandler(
             score: 1,
         });
         if (reviews.length === 0) {
-            response.status(404);
-            throw new Error("Sem análises disponíveis.");
+            response.status(404).json({ message: "Sem análises disponíveis." });
         }
 
         response.status(200).json(reviews);
     }
 );
 
-////private
-
-//post
+/**
+ * POST, AUTH REQUIRED - Create a review instance with given data.
+ *
+ * @param {Request} request - The HTTP request object containing the user ID, text, score, productOwner id and product id.
+ * @param {Response} response - The HTTP response object containing a conclusion message.
+ * @throws throws error if body isn't valid or if review author has not been found.
+ */
 export const submitReview = asyncHandler(
     async (request: Request, response: Response) => {
         if (!request.user || !request.body) {
@@ -125,26 +167,32 @@ export const submitReview = asyncHandler(
         ReviewValidator.validate(response, request.body);
         let { product, text, score, productOwner } = request.body;
 
-        let reviewer = (await User.findById(request.user).select({
+        let reviewer = await User.findById(request.user).select({
             password: -1,
-        })) as IUser;
+        });
         if (!reviewer) {
             response.status(404);
             throw new Error("Usuário não encontrado.");
         }
 
-        let review = await Review.create({
+        await Review.create({
             author: reviewer.id,
             product,
             text,
             score,
             productOwner,
         });
-        response.status(201).json(review);
+        response.status(201).json({ message: "Análise Publicada." });
     }
 );
 
-//delete
+/**
+ * DELETE, AUTH REQUIRED - Delete a review instance with given review id. It should be a valid ObjectId.
+ *
+ * @param {Request} request - The HTTP request object containing the user and review ID.
+ * @param {Response} response - The HTTP response object containing s conclusion message.
+ * @throws throws error if review author, review author is different from request user or if the review has not been found.
+ */
 export const deleteReview = asyncHandler(
     async (request: Request, response: Response) => {
         if (!request.params) {
@@ -153,7 +201,12 @@ export const deleteReview = asyncHandler(
         }
 
         let { id } = request.params;
-        let review = await Review.findById(request.params.id);
+        if (!Types.ObjectId.isValid(id)) {
+            response.status(400);
+            throw new Error("Dados Inválidos");
+        }
+
+        let review = await Review.findById(id);
         if (!review) {
             response.status(404);
             throw new Error("Análise não encontrada.");
@@ -170,12 +223,18 @@ export const deleteReview = asyncHandler(
             throw new Error("Não autorizado.");
         }
 
-        let deletedReview = await Review.findByIdAndDelete(review.id);
-        response.status(200).json(deletedReview);
+        await Review.findByIdAndDelete(review.id);
+        response.status(200).json({ message: "Análise Excluida." });
     }
 );
 
-//patch, need params
+/**
+ * PATCH, AUTH REQUIRED - Update a review instance with given review id. It should be a valid ObjectId.
+ *
+ * @param {Request} request - The HTTP request object containing the user and review ID, score and text.
+ * @param {Response} response - The HTTP response object containing s conclusion message.
+ * @throws throws error if review author, review author is different from request user or if the review has not been found.
+ */
 export const updateReview = asyncHandler(
     async (request: Request, response: Response) => {
         if (!request.params || !request.user || !request.body) {
@@ -204,10 +263,10 @@ export const updateReview = asyncHandler(
             throw new Error("Não autorizado.");
         }
 
-        let updatedReview = await Review.findByIdAndUpdate(review.id, {
+        await Review.findByIdAndUpdate(review.id, {
             text: text,
             score: score,
         });
-        response.status(200).json(updatedReview);
+        response.status(200).json({ message: "Análise Atualizada." });
     }
 );
