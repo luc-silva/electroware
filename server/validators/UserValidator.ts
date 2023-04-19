c25834140d76c4c30f845c0eaec9174a3fccf5d1
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-19 17:12:40 -0300

    Improve userValidator

--- server/validators/UserValidator.ts
+++ server/validators/UserValidator.ts
@@ -2,6 +2,8 @@ import { Response } from "express";
 import { Validator } from "../interface";
 
 interface UserBody {
+    email: string;
+    password: string;
     name: {
         first: string;
         last: string;
@@ -14,6 +16,8 @@ interface UserBody {
 }
 
 class UserValidator implements Validator {
+    private emailRegex = /.*@\w*.\.com/g;
+
     public validate(response: Response, requestBody: UserBody): void {
         let { name, location, description } = requestBody;
 
@@ -30,6 +34,28 @@ class UserValidator implements Validator {
         if (description.length > 250) {
             response.status(400);
             throw new Error("Campo descrição Inválido.");
+        }
+    }
+
+    public validateRegistration(response: Response, requestBody: UserBody) {
+        let { email, password, name, location, description } = requestBody;
+
+        if (!password || password.length < 8) {
+            response.status(400);
+            throw new Error("Campo senha inválido.");
+        }
+        if (!email || !email.match(this.emailRegex)) {
+            response.status(400);
+            throw new Error("Campo email inválido.");
+        }
+        if (!name || !name.first || name.first.length > 10 || name.last.length > 10) {
+            response.status(400);
+            throw new Error("Campo nome Inválido.");
+        }
+
+        if (!location.country || !location.state) {
+            response.status(400);
+            throw new Error("Campo estado ou país Inválidos.");
         }
     }
 }
