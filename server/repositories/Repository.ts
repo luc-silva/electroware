81e18e99588cf353f9a3aa9bfa77c9f3c1a911de
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-19 17:50:08 -0300

    Add repository layer and change userController
    Add repository layer.
    Add Repository superclass that has a method to validate MongoDB ObjectID
    Add the UserRepository and replace MongoDB queries with repository
    methods

---
+++ server/repositories/Repository.ts
@@ -0,0 +1,11 @@
+import { Types } from "mongoose";
+
+export class Repository {
+    protected validateObjectId(objectId: string) {
+        if (!Types.ObjectId.isValid(objectId)) {
+            throw new Error("Invalid Object ID.");
+        }
+    }
+}
+
+//
