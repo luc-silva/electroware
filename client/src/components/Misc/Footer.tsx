ce5df51d4d35464f01bec4d42ee5e8c919f24833
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-22 20:23:38 -0300

    Modify WebsiteLogo components
    Font color and size now inherits its parent-element.
    Also add website logo on footer

--- client/src/components/Misc/Footer.tsx
+++ client/src/components/Misc/Footer.tsx
@@ -1,54 +1,63 @@
 import { Link } from "react-router-dom";
 
 import styles from "./Footer.module.css";
+import { WebsiteLogo } from "./WebsiteLogo";
 
 export const Footer = () => {
     return (
         <footer className={styles["footer"]}>
-            <nav className={styles["footer__navigation"]}>
-                <ul className={styles["footer__navigation__menu"]}>
-                    <li>
-                        <Link
-                            to="https://github.com/luc-silva/electroware"
-                            target={"_blank"}
-                        >
-                            Código Fonte
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            to="https://github.com/luc-silva"
-                            target={"_blank"}
-                        >
-                            Github
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            to="https://www.linkedin.com/in/silva-luc/"
-                            target={"_blank"}
-                        >
-                            LinkedIn
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <div className={styles["footer__details"]}>
-                <div>
-                    <p>Criado por Lucas da Silva Santos.</p>
-                    <p>
-                        Tecnologias utilizadas: Typescript, ReactJS, Express e
-                        MongoDB.
-                    </p>
+            <div className={styles["footer__main"]}>
+                <nav className={styles["footer__navigation"]}>
+                    <ul className={styles["footer__navigation__menu"]}>
+                        <li>
+                            <Link
+                                to="https://github.com/luc-silva/electroware"
+                                target={"_blank"}
+                            >
+                                Código Fonte
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="https://github.com/luc-silva"
+                                target={"_blank"}
+                            >
+                                Github
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="https://www.linkedin.com/in/silva-luc/"
+                                target={"_blank"}
+                            >
+                                LinkedIn
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={styles["footer__details"]}>
+                    <div>
+                        <p>Criado por Lucas da Silva Santos.</p>
+                        <p>
+                            Tecnologias utilizadas: Typescript, ReactJS, Express
+                            e MongoDB.
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            Ícones por{" "}
+                            <a
+                                href="https://phosphoricons.com/"
+                                target={"_blank"}
+                            >
+                                Phosphor.
+                            </a>
+                        </p>
+                    </div>
                 </div>
-                <div>
-                    <p>
-                        Ícones por{" "}
-                        <a href="https://phosphoricons.com/" target={"_blank"}>
-                            Phosphor.
-                        </a>
-                    </p>
-                </div>
+            </div>
+            <div className={styles["footer__logo"]}>
+                <WebsiteLogo />
             </div>
         </footer>
     );
