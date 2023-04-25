663077ef6b3289bd30d5794764d2218aadbac3e2
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-25 15:21:12 -0300

    Add image carousel

--- client/src/components/Sections/HomeBanner.tsx
+++ client/src/components/Sections/HomeBanner.tsx
@@ -1,12 +1,67 @@
-import styles from "./HomeBanner.module.css"
+import { useEffect, useState } from "react";
+import styles from "./HomeBanner.module.css";
+
+import banner1 from "../../assets/images/electroware-banner-1.jpg";
+import banner2 from "../../assets/images/electroware-banner-2.jpg";
+import banner3 from "../../assets/images/electroware-banner-3.jpg";
+import { Circle } from "phosphor-react";
 
 export const HomeBanner = () => {
+    const images = [banner1, banner2, banner3];
+
+    let [counter, setCounter] = useState(0);
+    let [currentImage, setCurrentImage] = useState(images[0]);
+    let [seconds, setSeconds] = useState(4000);
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter((prev: number) => {
+                if (prev < images.length - 1) {
+                    return prev + 1;
+                }
+                return 0;
+            });
+        }, seconds);
+
+        return () => {};
+    }, []);
+    useEffect(() => {
+        setCurrentImage(images[counter]);
+    }, [counter]);
+
+    function handleIconClick(index: number) {
+        setCurrentImage(images[index]);
+    }
+
     return (
         <section className={styles["active-promos"]}>
-            <img
-                src={require("../../assets/electroware-banner-1.jpg")}
-                alt="homepage banner"
-            />
+            <img src={currentImage} alt="homepage banner" />
+            <div className={styles["control-panel"]}>
+                {images.map((item, index) => {
+                    if (counter === index) {
+                        return (
+                            <Circle
+                                size={15}
+                                key={index}
+                                onClick={() => {
+                                    handleIconClick(index);
+                                    setCounter(index)
+                                }}
+                            />
+                        );
+                    }
+                    return (
+                        <Circle
+                            size={10}
+                            key={index}
+                            onClick={() => {
+                                handleIconClick(index);
+                                setCounter(index)
+                            }}
+                        />
+                    );
+                })}
+            </div>
         </section>
     );
 };
--- client/src/global.d.ts
+++ client/src/global.d.ts
@@ -2,3 +2,7 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
+declare module '*.jpg' {
+  const value: any;
+  export default value;
+}
