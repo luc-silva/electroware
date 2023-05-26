f470a10e6e3990b80aa32e9b3f5adfbef0bc7169
Lucas Santos <lucas.silva1203@outlook.com>
2023-05-26 13:08:41 -0300

    Divide frontend into a different repository

--- client/src/components/Buttons/DeleteBtn.tsx
+++
@@ -1,9 +0,0 @@
-import { IconWeight, Trash } from "phosphor-react";
-
-export const DeletebBtn = ({
-    onClick,
-    sizing = 25,
-    weight = "bold",
-}: ISVGButtonComponentProps) => {
-    return <Trash size={sizing} weight={weight} onClick={onClick}/>;
-};
--- client/src/components/Misc/NothingAvailableDialog.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./NothingAvailableDialog.module.css";
-
-export const NothingAvailableDialog = ({ text }: { text: string }) => {
-    return <p className={styles["dialog"]}>{text}</p>;
-};
