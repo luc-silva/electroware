e5f0e62afc10640e1dc1f0cdef8aa285db05fcc8
Lucas Santos <lucas.silva1203@outlook.com>
2023-08-23 21:20:50 -0300

    chore: fix database transaction

--- database/config.js
+++ database/config.js
@@ -1,8 +1,18 @@
-db = db.getSiblingDB('project_electroware');
+db = db.getSiblingDB("project_electroware");
 
 db.createUser({
     user: "user",
     pwd: "pass",
-    roles: [ { role: "readWrite", db: "project_electroware"} ],
+    roles: [{ role: "readWrite", db: "project_electroware" }],
     passwordDigestor: "server",
-})
+});
+
+rs.initiate({
+    _id: "mongo-cluster",
+    members: [
+        { _id: 0, host: "mongodb1:27017" },
+        { _id: 1, host: "mongodb2:27018" }, 
+    ],
+});
+
+// rs.initiate({ _id: "mongo-cluster", members: [ { _id: 0, host: "mongodb1" }, { _id: 1, host: "mongodb2" }] });
